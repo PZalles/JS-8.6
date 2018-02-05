fa857ded714ed71d3885b2e8415db2eb7bf8806f
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-02-05 20:49:15 +0100

    Add new js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-var a = 8,
-	h = 10,
-	triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var a = 2,
+	b = 4,
+	value = (a*a) - (2*a*b) - (b*b);
+	console.log (value);
+
+	if (value >= 0) {
+		console.log ('Wynik dodatni');
+	} else if (value < 0) {
+		console.log ('Wynik ujemny');
+	} 
+
+	if (value == 0) {
+		console.log ('Wynik jest rowny 0');
+	} else if (value != 0) {
+		console.log ('Wynik jest rozny od zera');
+	}
